3ad6e7cdb359ccb6392b72f9ec822ae50d089dff
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-02-01 23:33:04 +0100

    Change bump2version to double quotes since black converts everything to double quotes

--- setup.py
+++ setup.py
@@ -36,8 +36,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
