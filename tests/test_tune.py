cfbf7747c92e10ce19a56bafee2c722de3234b77
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-02-01 13:31:40 +0100

    Fix reference to cli in test

--- tests/test_tune.py
+++ tests/test_tune.py
@@ -28,9 +28,8 @@ def test_content(response):
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli.cli)
     assert result.exit_code == 0
-    assert 'tune.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    help_result = runner.invoke(cli.cli, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
