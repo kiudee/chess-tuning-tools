97cb89a724045db5a4574a80c16126d5e326d8d4
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-02-10 14:33:14 +0100

    Bump version: 0.1.6 → 0.2.0

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kiudee/chess-tuning-tools",
-    version="0.1.6",
+    version="0.2.0",
     zip_safe=False,
 )
--- tune/__init__.py
+++ tune/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Karlson Pfannschmidt"""
 __email__ = "kiudee@mail.upb.de"
-__version__ = "0.1.6"
+__version__ = "0.2.0"
 
 from tune.utils import expected_ucb
 from tune.db_workers import TuningClient, TuningServer
