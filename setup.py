8a66fdcb8e3ad3142f44a752399bede4ff2937ab
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-03-02 16:47:26 +0100

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kiudee/chess-tuning-tools",
-    version="0.2.3",
+    version="0.3.0",
     zip_safe=False,
 )
