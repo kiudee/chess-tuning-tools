07ddfc56f0d9a0c28e15b3dd7faf02ab4190d5df
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-08-03 23:38:15 +0200

    Bump version: 0.4.0 → 0.5.0-beta.0

--- tune/__init__.py
+++ tune/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Karlson Pfannschmidt"""
 __email__ = "kiudee@mail.upb.de"
-__version__ = "0.4.0"
+__version__ = "0.5.0-beta.0"
 
 from tune.utils import expected_ucb
 from tune.db_workers import TuningClient, TuningServer
