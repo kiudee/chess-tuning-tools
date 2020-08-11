ad132fa92159a9647d3e6d136d8b1a24909a7bad
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-08-11 13:48:44 +0200

    Add comments and clean up a bit

--- examples/vastai/onstart.sh
+++ examples/vastai/onstart.sh
@@ -5,10 +5,17 @@ if [ "$TUNER_INSTALLED" != true ]; then
     mkdir tuning
     mkdir tuning/plots
 
+    # Install all the needed system packages needed for
+    #  * Compilation of LeelaChessZero and Stockfish
+    #  * Compilation of cutechess-cli
+    #  * Checking out git repositories
+    #  * Unpacking archives
     apt update
     apt install -y clang-6.0 gcc-8 g++-8 ninja-build pkg-config wget git libqtcore4 p7zip-full zlib1g-dev libpq-dev qt5-qmake qt5-default
 
-
+    # We install the chess-tuning-tools in a minimal python installation.
+    # With this we can avoid depending on the underlying system python
+    # installation.
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$HOME"/miniconda.sh
     chmod +x miniconda.sh
     bash ./miniconda.sh -b -p "$HOME"/miniconda
@@ -16,20 +23,22 @@ if [ "$TUNER_INSTALLED" != true ]; then
     echo PATH="$HOME"/miniconda/bin:"$PATH" >> "$HOME"/.bashrc
     rm "$HOME"/miniconda.sh
     . "$HOME"/miniconda/etc/profile.d/conda.sh
-    #conda init bash
-    #source $HOME/.bashrc
 
+    # Set up the environment for the chess-tuning-tools:
     conda create -y -n tuning python=3.8
     conda activate tuning
     pip install meson chess-tuning-tools
 
-
+    # Download a recent version of LeelaChessZero, compile it and copy it to the
+    # tuning folder:
     git clone https://github.com/LeelaChessZero/lc0.git
     cd lc0 || exit
     git checkout release/0.26
     CC=clang-6.0 CXX=clang++-6.0 ./build.sh
     cp build/release/lc0 "$HOME"/tuning/
 
+    # Download a recent version of Stockfish, compile it and copy it to the
+    # tuning folder:
     cd "$HOME" || exit
     git clone https://github.com/official-stockfish/Stockfish.git
     cd "$HOME"/Stockfish/src || exit
@@ -57,8 +66,11 @@ if [ "$TUNER_INSTALLED" != true ]; then
     wget https://cdn.discordapp.com/attachments/539960268982059008/723509619485442098/openings-dd.zip
     7za x -oopenings openings-dd.zip
 
-    # Download the neural network to use:
+    # Download the neural network to use for LeelaChessZero:
     wget https://training.lczero.org/get_network?sha=b30e742bcfd905815e0e7dbd4e1bafb41ade748f85d006b8e28758f1a3107ae3 -O 703810
+
+    # Download the neural network for Stockfish NNUE to use:
+    # TODO
 
     # Everything was installed successfully. Set environment variable and start
     # the tuning script:
