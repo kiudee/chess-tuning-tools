b0730f8c616e715b79c13803bd880170b2d4f6a9
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-08-12 16:15:41 +0200

    Allow cutechess-cli result parser to correctly parse cutechess-cli 1.2.0 output

--- tests/test_local.py
+++ tests/test_local.py
@@ -19,6 +19,29 @@ def test_parse_experiment_result():
     assert_almost_equal(score, 0.0)
     assert_almost_equal(error, 0.06, decimal=2)
 
+    # Test cutechess 1.2.0 output:
+    teststr = """Started game 1 of 4 (engine1 vs engine2)
+    Finished game 1 (engine1 vs engine2): 0-1 {Black mates}
+    Score of engine1 vs engine2: 0 - 1 - 0  [0.000] 1
+    Started game 2 of 4 (engine2 vs engine1)
+    Finished game 2 (engine2 vs engine1): 1/2-1/2 {Draw by stalemate}
+    Score of engine1 vs engine2: 0 - 1 - 1  [0.250] 2
+    Started game 3 of 4 (engine1 vs engine2)
+    Finished game 3 (engine1 vs engine2): 0-1 {Black mates}
+    Score of engine1 vs engine2: 0 - 2 - 1  [0.167] 3
+    Started game 4 of 4 (engine2 vs engine1)
+    Finished game 4 (engine2 vs engine1): 0-1 {Black mates}
+    Score of engine1 vs engine2: 1 - 2 - 1  [0.375] 4
+    ...      engine1 playing White: 0 - 2 - 0  [0.000] 2
+    ...      engine1 playing Black: 1 - 0 - 1  [0.750] 2
+    ...      White vs Black: 0 - 3 - 1  [0.125] 4
+    Elo difference: -88.7 +/- nan, LOS: 28.2 %, DrawRatio: 25.0 %
+    Finished match
+    """
+    score, error = parse_experiment_result(teststr, n_dirichlet_samples=1000)
+    assert_almost_equal(score, 1 / 9)
+    assert_almost_equal(error, 0.04, decimal=2)
+
 
 def test_reduce_ranges():
     space = normalize_dimensions([(0.0, 1.0), ("a", "b", "c")])
