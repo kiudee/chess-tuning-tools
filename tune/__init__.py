e9dbbf9dcf31ee34be8101c6738e8cb923cb03e0
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-08-13 19:46:34 +0200

    Import elo_to_prob and prob_to_elo in __init__

--- tune/__init__.py
+++ tune/__init__.py
@@ -5,23 +5,31 @@ __email__ = "kiudee@mail.upb.de"
 __version__ = "0.5.0-beta.3"
 
 from tune.io import InitStrings, load_tuning_config, parse_ranges
-from tune.local import parse_experiment_result, reduce_ranges, run_match
+from tune.local import (
+    elo_to_prob,
+    parse_experiment_result,
+    prob_to_elo,
+    reduce_ranges,
+    run_match,
+)
 from tune.plots import partial_dependence, plot_objective
 from tune.priors import roundflat
 from tune.utils import TimeControl, TimeControlBag, expected_ucb, parse_timecontrol
 
 __all__ = [
+    "elo_to_prob",
+    "expected_ucb",
     "InitStrings",
     "load_tuning_config",
-    "parse_ranges",
     "parse_experiment_result",
-    "run_match",
-    "reduce_ranges",
+    "parse_ranges",
+    "parse_timecontrol",
     "partial_dependence",
     "plot_objective",
+    "prob_to_elo",
+    "reduce_ranges",
     "roundflat",
-    "expected_ucb",
-    "parse_timecontrol",
+    "run_match",
     "TimeControl",
     "TimeControlBag",
 ]
