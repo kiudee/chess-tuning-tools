289c2e74304ac2ae8c905b67f77da697582836b2
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-08-15 14:15:03 +0200

    Set up sphinx_book_theme and myst-nb
    Add sphinx-autobuild dev dependency
    
    Update doc dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 import tune
 
@@ -31,22 +32,26 @@ import tune
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "myst_nb"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".ipynb": "myst-nb",
+    ".myst": "myst-nb",
+}
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'Chess Tuning Tools'
+project = "Chess Tuning Tools"
 copyright = "2020, Karlson Pfannschmidt"
 author = "Karlson Pfannschmidt"
 
@@ -69,10 +74,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -83,7 +88,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_book_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -94,13 +99,13 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'tunedoc'
+htmlhelp_basename = "tunedoc"
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -109,15 +114,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -127,9 +129,13 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'tune.tex',
-     'Chess Tuning Tools Documentation',
-     'Karlson Pfannschmidt', 'manual'),
+    (
+        master_doc,
+        "tune.tex",
+        "Chess Tuning Tools Documentation",
+        "Karlson Pfannschmidt",
+        "manual",
+    ),
 ]
 
 
@@ -137,11 +143,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'tune',
-     'Chess Tuning Tools Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "tune", "Chess Tuning Tools Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------
@@ -150,13 +152,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'tune',
-     'Chess Tuning Tools Documentation',
-     author,
-     'tune',
-     'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "tune",
+        "Chess Tuning Tools Documentation",
+        author,
+        "tune",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
-
-
-
