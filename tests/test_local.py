c354ed6b1f530e87f456e2537a1de7caff3830c0
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-06-26 17:21:54 +0200

    Test parse_experiment with a cutechess-cli log in shuffled order

--- tests/test_local.py
+++ tests/test_local.py
@@ -84,6 +84,32 @@ def test_parse_experiment_result():
     assert_almost_equal(score, -2.7958800173440745)
     assert_almost_equal(error, 1.9952678343378125)
 
+    # Test if the result is correct in case the order of finished games is not linear.
+    # This can happen with concurrency > 1
+    teststr = """Started game 1 of 4 (engine1 vs engine2)
+    Started game 2 of 4 (engine2 vs engine1)
+    Started game 3 of 4 (engine1 vs engine2)
+    Started game 4 of 4 (engine2 vs engine1)
+    Finished game 4 (engine2 vs engine1): 0-1 {Black mates}
+    Score of engine1 vs engine2: 1 - 0 - 0  [0.375] 1
+    Finished game 1 (engine1 vs engine2): 1/2-1/2 {Draw by stalemate}
+    Score of engine1 vs engine2: 1 - 0 - 1  [0.000] 2
+    Finished game 2 (engine2 vs engine1): 1-0 {White mates}
+    Score of engine1 vs engine2: 1 - 1 - 1  [0.250] 3
+    Finished game 3 (engine1 vs engine2): 0-1 {Black mates}
+    Score of engine1 vs engine2: 1 - 2 - 1  [0.167] 4
+    ...      engine1 playing White: 0 - 2 - 0  [0.000] 2
+    ...      engine1 playing Black: 1 - 0 - 1  [0.750] 2
+    ...      White vs Black: 0 - 3 - 1  [0.125] 4
+    Elo difference: -88.7 +/- nan, LOS: 28.2 %, DrawRatio: 25.0 %
+    Finished match
+    """
+    score, error = parse_experiment_result(
+        teststr, n_dirichlet_samples=1000, random_state=0
+    )
+    assert_almost_equal(score, 0.38764005203222596)
+    assert_almost_equal(error, 0.6255020676255081)
+
 
 def test_reduce_ranges():
     space = normalize_dimensions([(0.0, 1.0), ("a", "b", "c")])
