90eb35673cd10151ffba6fc5d781f8e523f49891
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-07-28 10:57:57 +0200

    Add unit tests for initialize_data

--- tests/test_local.py
+++ tests/test_local.py
@@ -1,7 +1,9 @@
+import numpy as np
+import pytest
 from numpy.testing import assert_almost_equal
 from skopt.utils import normalize_dimensions
 
-from tune.local import parse_experiment_result, reduce_ranges
+from tune.local import initialize_data, parse_experiment_result, reduce_ranges
 
 
 def test_parse_experiment_result():
@@ -121,3 +123,52 @@ def test_reduce_ranges():
     assert tuple(x_new) == ((0.0, "a"), (1.0, "c"))
     assert tuple(y_new) == (0.0, 3.0)
     assert tuple(noise_new) == (0.1, 0.4)
+
+
+def test_initialize_data(tmp_path):
+    # Test basic functionality without resume:
+    X, y, noise, iteration = initialize_data(
+        parameter_ranges=[(0.0, 1.0)], data_path=None, resume=False,
+    )
+    assert len(X) == 0
+    assert len(y) == 0
+    assert len(noise) == 0
+    assert iteration == 0
+
+    # Create a temporary file for testing:
+    testfile = tmp_path / "data.npz"
+    X_in = np.array([[0.0], [0.5], [1.0]])
+    y_in = np.array([1.0, -1.0, 0.0])
+    noise_in = np.array([0.3, 0.2, 0.5])
+    np.savez_compressed(testfile, X_in, y_in, noise_in)
+
+    # Check if resume=False is recognized correctly
+    # (outputs should be empty despite data_path being given):
+    X, _, _, _ = initialize_data(
+        parameter_ranges=[(0.0, 1.0)], data_path=testfile, resume=False,
+    )
+    assert len(X) == 0
+
+    # Check if we get the data back we saved with resume=True:
+    X, y, noise, iteration = initialize_data(
+        parameter_ranges=[(0.0, 1.0)], data_path=testfile, resume=True,
+    )
+    assert iteration == 3
+    assert np.allclose(X, X_in)
+    assert np.allclose(y, y_in)
+    assert np.allclose(noise, noise_in)
+
+    # Check if we get the correct subset, if we reduce the parameter range:
+    X, y, noise, iteration = initialize_data(
+        parameter_ranges=[(0.0, 0.5)], data_path=testfile, resume=True,
+    )
+    assert iteration == 2
+    assert np.allclose(X, np.array([[0.0], [0.5]]))
+    assert np.allclose(y, np.array([1.0, -1.0]))
+    assert np.allclose(noise, np.array([0.3, 0.2]))
+
+    # Check if the ValueError is raised correctly:
+    with pytest.raises(ValueError):
+        _ = initialize_data(
+            parameter_ranges=[(0.0, 1.0)] * 2, data_path=testfile, resume=True,
+        )
