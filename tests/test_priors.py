a141240bf6ee028963ab80b7087fe7d54373a7ae
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-08-03 18:23:06 +0200

    Add test for make_invgamma_prior

--- tests/test_priors.py
+++ tests/test_priors.py
@@ -1,11 +1,23 @@
 import numpy as np
-from numpy.testing import assert_almost_equal
+from pytest import approx, raises
 
-from tune.priors import roundflat
+from tune.priors import make_invgamma_prior, roundflat
 
 
 def test_roundflat():
-    assert_almost_equal(roundflat(0.3), 0.0, decimal=0.1)
-
+    assert roundflat(0.3) == approx(0.0, abs=1e-6)
     assert roundflat(0.0) == -np.inf
     assert roundflat(-1.0) == -np.inf
+
+
+def test_make_invgamma_prior():
+    prior = make_invgamma_prior()
+    assert prior.kwds["a"] == approx(8.919240823584246)
+    assert prior.kwds["scale"] == approx(1.7290248731437994)
+
+    with raises(ValueError):
+        make_invgamma_prior(lower_bound=-1e-10)
+    with raises(ValueError):
+        make_invgamma_prior(upper_bound=-1e-10)
+    with raises(ValueError):
+        make_invgamma_prior(lower_bound=0.5, upper_bound=0.1)
