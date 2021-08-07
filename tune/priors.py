f80fa3de000e465cf807c0d46954b5892cd26d80
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-08-07 23:00:23 +0200

    Add create_priors function

--- tune/priors.py
+++ tune/priors.py
@@ -1,11 +1,12 @@
 import warnings
+from typing import Callable, List
 
 import numpy as np
 from scipy.optimize import curve_fit
-from scipy.stats import invgamma
+from scipy.stats import halfnorm, invgamma
 from scipy.stats._distn_infrastructure import rv_frozen  # noqa
 
-__all__ = ["make_invgamma_prior", "roundflat"]
+__all__ = ["make_invgamma_prior", "roundflat", "create_priors"]
 
 
 def roundflat(x, a_low=2.0, a_high=8.0, d_low=0.005, d_high=1.2):
@@ -71,3 +72,60 @@ def make_invgamma_prior(
             [lower_bound, upper_bound],
         )
     return invgamma(a=a_out, scale=scale_out)
+
+
+def create_priors(
+    n_parameters: int,
+    signal_scale: float = 4.0,
+    lengthscale_lower_bound: float = 0.1,
+    lengthscale_upper_bound: float = 0.5,
+    noise_scale: float = 0.0006,
+) -> List[Callable[[float], float]]:
+    """Create a list of priors to be used for the hyperparameters of the tuning process.
+
+    Parameters
+    ----------
+    n_parameters : int
+        Number of parameters to be optimized.
+    signal_scale : float
+        Prior scale of the signal (standard deviation) which is used to parametrize a
+        half-normal distribution.
+    lengthscale_lower_bound : float
+        Lower bound of the inverse-gamma lengthscale prior. It marks the point at which
+        1 % of the cumulative density is reached.
+    lengthscale_upper_bound : float
+        Upper bound of the inverse-gamma lengthscale prior. It marks the point at which
+        99 % of the cumulative density is reached.
+    noise_scale : float
+        Prior scale of the noise (standard deviation) which is used to parametrize a
+        half-normal distribution.
+
+    Returns
+    -------
+    list of callables
+        List of priors in the following order:
+         - signal prior
+         - lengthscale prior (n_parameters times)
+         - noise prior
+    """
+    if signal_scale <= 0.0:
+        raise ValueError(
+            f"The signal scale needs to be strictly positive. Got {signal_scale}."
+        )
+    if noise_scale <= 0.0:
+        raise ValueError(
+            f"The noise scale needs to be strictly positive. Got {noise_scale}."
+        )
+    signal_prior = halfnorm(scale=signal_scale)
+    lengthscale_prior = make_invgamma_prior(
+        lower_bound=lengthscale_lower_bound, upper_bound=lengthscale_upper_bound
+    )
+    noise_prior = halfnorm(scale=noise_scale)
+
+    priors = [lambda x: signal_prior.logpdf(np.sqrt(np.exp(x))) + x / 2.0 - np.log(2.0)]
+    for _ in range(n_parameters):
+        priors.append(lambda x: lengthscale_prior.logpdf(np.exp(x)) + x)
+    priors.append(
+        lambda x: noise_prior.logpdf(np.sqrt(np.exp(x))) + x / 2.0 - np.log(2.0)
+    )
+    return priors
